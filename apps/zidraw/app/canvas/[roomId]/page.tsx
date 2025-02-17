da81344b82130f22c36a380445c2234a7fdc8ca7
prayush <prayushgiri@gmail.com>
2025-02-17 14:46:38 +0530

    server fix

--- apps/zidraw/app/canvas/[roomId]/page.tsx
+++ apps/zidraw/app/canvas/[roomId]/page.tsx
@@ -1,13 +1,7 @@
 import { RoomCanvas } from '@/components/Canvas/RoomCanvas';
 
-export default async function CanvasPage({ params }: {
-    params: {
-        roomId: string
-    }
-}) { 
-        const roomId = (await params).roomId;
+export default async function CanvasPage({ params }: { params: { roomId: string } }) {
+    const roomId = params.roomId;
 
-        return(
-            <RoomCanvas roomId={roomId} />
-        )
+    return <RoomCanvas roomId={roomId} />;
 }
