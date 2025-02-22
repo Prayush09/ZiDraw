67e1e1218c1f2c894dea5413ec8cdffcf090aaf7
prayush <prayushgiri@gmail.com>
2025-02-22 17:11:53 +0530

    added cors

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -13,7 +13,7 @@ app.use(express.json());
 app.set('trust proxy', true);
 
 app.use(cors({
-    origin: '*',
+    origin: ["http://dev-http.zidraw.com", "http://localhost:3000", "http://app-http.zidraw.com"],
     credentials: true
 }));
 
